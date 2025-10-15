740ff521fa38bef787c66733c5e2059e15e34b69
Yash Parkhi <yashparkhi123@gmail.com>
2025-10-15 21:58:05 +0530

    Fix test to match updated HTML title

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_index_route():
     client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Student Information" in response.data
+    assert b"Student Info" in response.data
